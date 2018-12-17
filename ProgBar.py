8df15faa03fce71f9fbc11ed1ab9ada54dd580a6
Paulo Raposo <pauloj.raposo@outlook.com>
2018-12-17 00:53:05 -0500

    Bar uses / and - characters by default now, with the prior, less commonly available full and empty block characters being chosen if the user passes unicode=True.

--- ProgBar.py
+++ ProgBar.py
@@ -8,23 +8,30 @@
 #   ^^-^^
 
 
-def ProgressBar(percent, prefix=None, notches=50, numericalpercent=True):
+def ProgressBar(percent, prefix=None, notches=50, numericalpercent=True, unicode=False):
 
     """Accepting a number between 0.0 and 1.0 [percent], returns a string containing a UTF-8
     representation of a progress bar of x segments [notches] to the screen, along with an
     optional indication of the progress as the given percentage rounded to two places
     [numericalpercent], and, if given one, a custom string preceding the progress bar
-    [prefix]."""
+    [prefix]. By default, common slashes and dashes are used to draw the bar's full and
+    empty portions, respectively; [unicode] can be set to True to use full and empty blocks
+    from the Unicode character set instead, which are not defined in all fonts."""
 
     outString = u"" # Unicode string.
     if prefix:
         prefix = "{} ".format(prefix)
         outString = outString + prefix
     x_of_notches = int(round(percent * notches))
+    fullSegment  = "/"
+    blankSegment = "-"
+    if unicode:
+        fullSegment  = "\u25AE" # Full block in Unicode
+        blankSegment = "\u25AF" # Empty block in Unicode
     for i in range(x_of_notches):
-        outString = outString + "\u25AE" # Full block
+        outString = outString + fullSegment # Full block
     for i in range(notches - x_of_notches):
-        outString = outString + "\u25AF" # Empty block
+        outString = outString + blankSegment
     if numericalpercent:
         outString = outString + " [{}%]".format(str(round(percent * 100, 2)))
     return outString
@@ -41,7 +48,7 @@ if __name__ == "__main__":
     barLength = termWidth - 30
     for i in range(steps):
         progress = (i + 1) / steps
-        print(ProgressBar(progress, aPrefix, barLength), end="\r", flush=True)
+        print(ProgressBar(progress, aPrefix, barLength, True), end="\r", flush=True)
         if i == steps - 1:
             print()
         else:
